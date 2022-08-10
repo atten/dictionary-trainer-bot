0ac34dee2f5477072ebd519614e8d23f39f669e6
atten <art@force.fm>
2022-08-10 10:41:05 +0300

    fix id out of range

--- bot/apps.py
+++ bot/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BotAppConfig(AppConfig):
     name = 'bot'
-    default_auto_field = 'django.db.models.AutoField'
+    default_auto_field = 'django.db.models.BigAutoField'
